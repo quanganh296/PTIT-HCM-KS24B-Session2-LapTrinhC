89d9f68bbe48c846dcfad9bde9b6ea675a10f7de
Quater <102786860+quanganh296@users.noreply.github.com>
2024-12-10 11:57:43 +0700

    Add files via upload

---
+++ TinhTong.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include<iomanip>
+#include<math.h> 
+using namespace std;
+int main()
+{
+int a,b;
+printf("Ban hay nhap bien a: ") ;
+scanf("%d",&a); 
+printf("Ban hay nhap bien b: ") ;
+scanf("%d",&b);
+
+printf("a =%d va b=%d  \n",a,b ) ;
+int tong=0; 
+tong+=a+b; 
+printf("tong 2 so là: %d \n",tong); 
+
+
+	return 0;
+}
